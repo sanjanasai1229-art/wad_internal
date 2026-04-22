99e8124ab8d78b1d5765281a09c48376da9746f3
Sanjana Sai <sanjanasai1229@gmail.com>
2026-04-22 12:15:48 +0530

    Fix bug in login logic and update README

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, session, request, redirect
 app = Flask(__name__)
 app.secret_key="secret123"
+users = {
+    "student":"123",
+    "user1":"245",
+    "user2":"1243",
+    "user3":"999",   
+    "user4":"456",  
+    "user5":"789"  
+}
 @app.route("/")
 def home():
     if "user" in session:
@@ -8,9 +16,11 @@ def home():
     return "<form method='POST' action='/login'>User:<input name='u'> Pass:<input name='p' type='password'><button>Login</button></form>"
 @app.route("/login", methods=["POST"])
 def login():
-    if request.form["u"]=="student" and request.form["p"]=="123":
-        session["user"]="student"
-        session["cart"]=[]
+    username = request.form["u"]
+    password = request.form["p"]
+    if username in users and users[username] == password:
+        session["user"] = username
+        session["cart"] = []
         return redirect("/")
     return "Invalid!"
 @app.route("/add/<item>")
@@ -22,5 +32,5 @@ def add(item):
 def logout():
     session.clear()
     return redirect("/")
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
